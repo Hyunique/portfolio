17c38654edac3fe9f0b7f4b085e09f156148ed6d
Elena <kunstvanelena@gmail.com>
2023-08-14 16:42:14 +0200

    implement auto scroll to nav items

--- app.js
+++ app.js
@@ -26,21 +26,34 @@ function toggleThemeBtn() {
 }
 
 const scrollTo = document.querySelector(".arrow");
+const homeSection = document.querySelector(".landing");
 const aboutSection = document.querySelector(".about");
 const portfolioSection = document.querySelector(".projects-container");
 const contactSection = document.querySelector(".contact-container");
-const aboutcoords = aboutSection.getBoundingClientRect();
-const portfoliocoords = portfolioSection.getBoundingClientRect();
-const contactcoords = contactSection.getBoundingClientRect();
+// const aboutcoords = aboutSection.getBoundingClientRect();
+// const portfoliocoords = portfolioSection.getBoundingClientRect();
+// const contactcoords = contactSection.getBoundingClientRect();
 
 scrollTo.addEventListener("click", () => {
   aboutSection.scrollIntoView({ behavior: "smooth" });
 });
-// console.log(aboutcoords);
-// console.log(e.target.getBoundingClientRect());
-// console.log(window.pageYOffset);
-// window.scrollTo({
-//   left: aboutcoords.left,
-//   top: aboutcoords.top + window.pageYOffset,
-//   behavior: "smooth",
-// });
+
+const navList = document.querySelector(".navList");
+const navItems = document.querySelectorAll(".navItem");
+navList.addEventListener("click", (e) => {
+  const menuName = e.target.textContent;
+  switch (menuName) {
+    case "About":
+      aboutSection.scrollIntoView({ behavior: "smooth" });
+      break;
+    case "Portfolio":
+      portfolioSection.scrollIntoView({ behavior: "smooth" });
+      break;
+    case "Contact":
+      contactSection.scrollIntoView({ behavior: "smooth" });
+      break;
+    case "Home":
+      homeSection.scrollIntoView({ behavior: "smooth" });
+      break;
+  }
+});
