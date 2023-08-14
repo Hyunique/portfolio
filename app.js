4e02b1cf1d7ccca0e6553451f6196b3d9d91517c
Elena <kunstvanelena@gmail.com>
2023-08-14 16:57:31 +0200

    implement hidden nav bar on scroll

--- app.js
+++ app.js
@@ -57,3 +57,14 @@ navList.addEventListener("click", (e) => {
       break;
   }
 });
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector("header").style.top = "0";
+  } else {
+    document.querySelector("header").style.top = "-70px";
+  }
+  prevScrollpos = currentScrollPos;
+};
